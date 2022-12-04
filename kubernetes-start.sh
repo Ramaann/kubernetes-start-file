ceb970837aa2de855f2951dbc85d75ce03b128e4
root <root@ip-172-31-36-163.ap-south-1.compute.internal>
2022-12-04 16:03:37 +0000

    kubernetes-updated

--- kubernetes-start.sh
+++ kubernetes-start.sh
@@ -8,3 +8,14 @@ set -e
 sudo sh kops-binary.sh
 sudo sh kubectl-binary.sh
 
+#aws configure
+#export AWS_ACCESS_KEY_ID=AKIAVSSHIJ2IXEEVG7N7
+
+#export AWS_SECRET_ACCESS_KEY=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
+
+#export NAME=raman.k8s.local
+
+#export KOPS_STATE_STORE=s3://ramm-bucket
+#ssh-keygen
+
+#kops create cluster --zones ap-south-1a ${NAME}
